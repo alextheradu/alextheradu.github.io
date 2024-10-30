266f3433693825f4927b579b112f3a5612c710b8
Alex Radu <cooldudev.super@gmail.com>
2024-10-29 20:32:44 -0400

    Updated InfoTech Logs

---
+++ logs/infotech/chat/index.js
@@ -0,0 +1,27 @@
+function submit() {
+    let div = document.getElementById('chat');
+
+    let text = $("#input").val();
+
+    let chat = document.createElement("p",);
+    chat.innerHTML = text;
+    div.appendChild(chat);
+    $post("https://blark.it/tcsrw/message_app/fetch_messages.php", {
+        message: text,
+        from: "placeholder",
+        to: "infotech",
+    },
+    )
+}
+
+$.get("https://blark.it/tcsrw/message_app/fetch_messages.php",
+    (data) => {
+        for(let i = 0; i < data.length; i++) {
+            
+            let message = document.createElement("p");
+
+            message.innerHTML = data[i].message;
+            document.getElementById('chat').appendChild(message);
+        }
+    }
+);
