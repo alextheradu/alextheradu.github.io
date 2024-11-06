2e6814e73039a348d01dbff82fc7970f91b8bffa
Alex Radu <cooldudev.super@gmail.com>
2024-11-06 16:02:58 -0500

    Update index.html

--- subpages/index.js
+++ subpages/index.js
@@ -1,3 +1,10 @@
-function wrongPass() {
-    window.location.href = "/about-me/";
+function checkPassword() {
+    var password = prompt("Please enter the password:");
+    if (password === "yourPassword") {
+        document.getElementById("content").style.display = "block";
+    } else {
+        alert("Incorrect password!");
+        window.location.href = "https://www.example.com"; // Redirect to another page
     }
+}
+window.onload = checkPassword;
